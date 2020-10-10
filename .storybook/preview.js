45ed1098d8a7b46be73c4023518040ab9e13ef9f
hovonako <khoahoang@kms-technology.com>
2020-10-10 23:14:38 +0700

    Set default viewport

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,17 +1,11 @@
 import React from 'react'
+
 import { addDecorator } from '@storybook/react'
-import { withInfo } from '@storybook/addon-info'
+
 import Layout from './layout'
-import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport'
 
 const customViewports = {
-  extensionView: {
-    name: 'ExtensionView',
-    styles: {
-      width: '380px',
-      height: '600px',
-    },
-  },
+
 }
 
 addDecorator((storyFn) => <Layout>{storyFn()}</Layout>)
@@ -19,7 +13,15 @@ addDecorator((storyFn) => <Layout>{storyFn()}</Layout>)
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
   viewport: {
-    viewports: { ...MINIMAL_VIEWPORTS, ...customViewports },
-    defaultViewport: 'ExtensionView',
+    viewports: {
+      extensionView: {
+        name: 'ExtensionView',
+        styles: {
+          width: '380px',
+          height: '600px',
+        },
+      },
+    },
+    defaultViewport: 'extensionView',
   },
 }
