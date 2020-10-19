45ba3a2dffc9e6d43b7ddb0955833e46661a6ba4
Tran Hoang <tranhoang.finizz@gmail.com>
2020-10-19 11:50:29 +0700

    change

--- src/popup/app-router.tsx
+++ src/popup/app-router.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { useSettingStore } from 'popup/stores/features/settings'
+import { useIsAlreadyHaveWallet } from '../queries/use-is-already-have-wallet'
 
 import { HomePage } from './pages/home/home-page'
 import { WelcomePage } from './pages/Welcome/welcome-page'
 // import { AddAccountPanel } from './pages/home/components/add-account/add-account-panel'
 
 export const AppRouter = () => {
-  const selectedAccount = useSettingStore((s) => s.selectAccountName)
+  // const selectedAccount = useSettingStore((s) => s.selectAccountName)
+  const isHaveAccount = useIsAlreadyHaveWallet()
 
-  if (!selectedAccount) {
+  if (!isHaveAccount) {
     return (
       <Router>
         <>
