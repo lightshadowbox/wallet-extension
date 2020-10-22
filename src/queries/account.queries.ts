a42a1ff2872c75c4e08af5a78be5dd10f81918fd
0x1905 <khoahoang.tech@gmail.com>
2020-10-22 09:25:06 +0700

    Update account

--- src/queries/account.queries.ts
+++ src/queries/account.queries.ts
@@ -1,9 +1,7 @@
-import { PrivacyToken } from 'incognito-sdk/build/web/module/src/walletInstance/token'
-import { map } from 'lodash'
 import { AccountModelType, serializeAccount } from 'models/account-model'
-import { TokenModelType } from 'models/token-model'
 import { useSettingStore } from 'popup/stores/features/settings'
 import { useQuery } from 'react-query'
+import { getAccountRuntime } from 'services/wallet'
 import { useGetWallet } from './wallet.queries'
 
 export const useGetAccount = () => {
@@ -13,7 +11,7 @@ export const useGetAccount = () => {
   return useQuery<AccountModelType>(
     [useGetAccount.name, selectedAccount],
     async () => {
-      const accountInstance = wallet.masterAccount.getAccountByName(selectedAccount)
+      const accountInstance = await getAccountRuntime(selectedAccount)
       const accountSerizialed = await serializeAccount(accountInstance)
       return accountSerizialed
     },
