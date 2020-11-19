72752f0d1d804d959e4ec6b3d78284b304a3ebaa
Shinre H <shinre@emageai.com>
2020-11-19 20:56:55 +0700

    :lipstick: chore: decorated a divider at the bottom of header

--- src/popup/components/header/header.tsx
+++ src/popup/components/header/header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import { FaButton } from 'popup/components/button'
-import { Label } from '@fluentui/react'
+import { Label, VerticalDivider } from '@fluentui/react'
 import styled from './header.module.css'
 
 type Props = {
@@ -13,10 +13,9 @@ type Props = {
 export const Header: React.FC<Props> = ({ title, icon, dismissPanel, iconColor }) => (
   <header>
     <div className={`${styled.headerLayout__main}`}>
-      <div className={classNames('')}>
-        <FaButton className={styled.headerLayout__icon} onClick={dismissPanel} iconProps={{ iconName: icon }} iconColor={iconColor || '#1d1d1d'} />
-        <Label className={styled.headerLayout__title}>{title}</Label>
-      </div>
+      <FaButton className={styled.headerLayout__icon} onClick={dismissPanel} iconProps={{ iconName: icon }} iconColor={iconColor || '#1d1d1d'} />
+      <Label className={styled.headerLayout__title}>{title}</Label>
+      <div className={styled.headerLayout__divider} />
     </div>
   </header>
 )
